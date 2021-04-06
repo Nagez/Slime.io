70b31358b2caa4cd2e41f49224db38633b346e3d
Nagez <81879748+Nagez@users.noreply.github.com>
2021-04-06 13:05:33 +0300

    da

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
