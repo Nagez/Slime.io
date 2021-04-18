06be5219f805eb19cbd1676b470d44a47ee31a4a
narmeen-kh99 <narmeen.kh99@gmail.com>
2021-04-18 20:19:31 +0300

    done from the errors and update script

---
+++ Assets/Scripts/FollwThePath.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollwThePath : MonoBehaviour
+{
+    public bool moveAllowed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+      //  if(moveAllowed)
+    }
+}
--- Assets/Scripts/game.cs
+++ Assets/Scripts/game.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class game : MonoBehaviour
 {
-    private static int diceSideThrown = 0;
+    public static int diceSideThrown = 0;//num move points
     public static bool gameOver = false;
+    private static GameObject player1MoveText, player2MoveText;
+    private static GameObject player1, player2;
+    
     void Start()
     {
-
+        player1.GetComponent<FollwThePath>().moveAllowed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public static void MovePlayer(int playerToMove)
+    {
+        switch(playerToMove)
+        {
+            case 1:
+                player1.GetComponent<FollwThePath>().moveAllowed = true;
+                break;
+        }
     }
 }
