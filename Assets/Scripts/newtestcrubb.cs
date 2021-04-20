7f873296f8ea0245bcfbbbee13baebeaec17a266
Nagez <81879748+Nagez@users.noreply.github.com>
2021-04-20 11:00:56 +0300

    dice and changes

--- Assets/Scripts/newtestcrubb.cs
+++ Assets/Scripts/newtestcrubb.cs
@@ -7,8 +7,8 @@ public class newtestcrubb : MonoBehaviour
     private Sprite[] dicesides;
     private int[] results;
     private SpriteRenderer rend;//to change sides of dice images
-    private int whosTurn = 1;//who play-כהתחלה 1 הוא משתמש 1
-    private int numOfPlayers;
+    //private int whosTurn = 1;//who play-כהתחלה 1 הוא משתמש 1
+    //private int numOfPlayers;
     private bool coroutineAllowed = true;
     private void Start()
     {
@@ -16,17 +16,26 @@ public class newtestcrubb : MonoBehaviour
         dicesides = Resources.LoadAll<Sprite>("dice/");
         rend.sprite = dicesides[1];//to be in the begining of the game the num 6 n th dice side
     }
-    private void onMouseDown()
+
+    private void OnMouseDown()
     {
+        Debug.Log("pressed1");
+
         //if (!game.gameOver && coroutineAllowed)
         if (coroutineAllowed)
+        {
+            Debug.Log("pressed");
             StartCoroutine("RollTheDice");//אם המשחק לא נגמר  אז זורקים קוביה מפעלים פונקציה
+        }
+
     }
-    private IEnumerator RollThDice()
+
+    private IEnumerator RollTheDice()
     {
         coroutineAllowed = false;
         int randomDiceSide = 0;
         int numofThrown = 0;
+
         while (randomDiceSide != 1 && randomDiceSide != 2 && randomDiceSide != 3)
         {
             for (int i = 0; i <= 20; i++)
@@ -37,7 +46,6 @@ public class newtestcrubb : MonoBehaviour
             }
             results[numofThrown] = randomDiceSide; //array of results
             numofThrown++;
-
         }
     }
 }
