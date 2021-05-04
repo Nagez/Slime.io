7897f4ed5cf5cff2c2c445d747495df0fb043f87
AlinaBlo <82020749+AlinaBlo@users.noreply.github.com>
2021-05-04 13:37:52 +0300

    Fix Prob

--- Assets/Alinas Experements/Script/SlimeSpawner_A.cs
+++ Assets/Alinas Experements/Script/SlimeSpawner_A.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeSpawner : MonoBehaviour
+public class SlimeSpawner_A : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Alinas Experements/Script/TurnControl_A.cs
+++ Assets/Alinas Experements/Script/TurnControl_A.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnControl : MonoBehaviour
+public class TurnControl_A : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
